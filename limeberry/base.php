cd36d8ff419f7936c36a2842f2491d068cfb3608
inisik <inisik0820@gmail.com>
2018-04-27 00:51:08 -0400

    Cleanup.

--- limeberry/base.php
+++ limeberry/base.php
@@ -1,44 +1,24 @@
 <?php
-
 /**
-*	Limeberry Framework
-*	
-*	a php framework for fast web development.
-*	
-*	@package Limeberry Framework
-*	@author Sinan SALIH
-*	@copyright Copyright (C) 2018 Sinan SALIH
-*	
-**/
-
-
-
-$application_folder = "application";
-$application_is_root = false;
-
-$application_is_urlsecure = false;
-$application_unwanted_params = array();
-
-$application_install_url = "localhost";
-$application_static_routes = array();
-
-$application_name = "Limeberry Application";
-$application_version = "1.0.0";
-$application_description = "Your description for the application";
-   
-
-$application_query_data = array();
-
-/**
- * @ignore
+ * Limeberry Framework
+ *
+ *	A php framework for fast web development.
+ *
+ *	@package Limeberry Framework
+ *	@author Sinan SALIH
+ *	@copyright Copyright (C) 2018 Sinan SALIH
  */
-define('DS', DIRECTORY_SEPARATOR);
-/**
- * @ignore
- */
-define('ROOT', dirname(dirname(__FILE__)));
 
+define('APP_FOLDER', 'application');
+define('APP_IS_ROOT', false);
 
+define('APP_IS_URLSECURE', false);
+define('APP_UNWANTED_PARAMS', array());
 
+define('APP_INSTALL_LOCATION', 'localhost');
+define('APP_STATIC_ROUTES', array());
 
-?>
+define('APP_NAME', 'Limeberry Application');
+define('APP_DESC', 'Your description for the application.');
+
+define('APP_QUERY_DATA', array());
